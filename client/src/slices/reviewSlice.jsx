0e230b8798edd04d4350fc28c2da2f7ce2e368bd
Michael Del Pape <m4delpape@gmail.com>
2023-05-04 14:55:27 -0700

    Completed basic initial get function to retriece data based on the current product id

--- client/src/slices/reviewSlice.jsx
+++ client/src/slices/reviewSlice.jsx
@@ -1,9 +1,45 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from "axios";
 
-const initialState = {
+const baseAPIURL = "https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/";
 
+const initialState = {
+  product_id: '',
+  reviews: [],
+  recommended: '',
+  characteristsics: {
+    Size: {
+      id: '',
+      value: ''
+    },
+    Width: {
+      id: '',
+      value: ''
+    },
+    Comfort: {
+      id: '',
+      value: ''
+    },
+  }
 };
+
+export const getInitialData = createAsyncThunk('reviews', async(id, thunkAPI) => {
+  axios.get(baseAPIURL, {
+    params: {
+      page: 1,
+      count: 5,
+      sort: 'newest',
+      product_id: id
+    }
+  }).then((res) => {
+    console.log(res.data);
+    // set the states with the returned data
+  }.catch((err) => {
+    console.log(err);
+  })
+  )
+})
+
 
 const reviewSlice = createSlice({
   name: review,
