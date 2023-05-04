626b5da30055eb753db388ef4cba35dfecad9942
Jacqueline-Kelly <Jacqueline.kelly.2016@gmail.com>
2023-05-04 14:26:42 -0700

    getStyles function

--- client/src/slices/productSlice.jsx
+++ client/src/slices/productSlice.jsx
@@ -25,10 +25,11 @@ const initialState = {
 
   },
   isError: false,
+  errorMessage: '',
   isLoading: false,
 };
 
-// Retrieves a list of products. API default query params gets first page and five results. Returns an array of products
+// Retrieves a list of product overview objects. API default query params gets first page and five results. Returns an array of products
 export const getInitialData = createAsyncThunk('product/getInitialData', async(_, thunkAPI) => {
   try {
     axios.get(`${baseAPIURL}/products`).then((res) => res.data);
@@ -37,6 +38,7 @@ export const getInitialData = createAsyncThunk('product/getInitialData', async(_
   }
 })
 
+// Retrieves a single product object from the path parameter, productId
 export const getSpecificProduct = createAsyncThunk('product/getSpecificProduct', async(productId, thunkAPI) => {
   try {
     axios.get(`${baseAPIURL}/product/${productId}`).then(res => res.data)
@@ -45,6 +47,7 @@ export const getSpecificProduct = createAsyncThunk('product/getSpecificProduct',
   }
 });
 
+// Retrieves an array of style objects from the path parameter, productId
 export const getStyles = createAsyncThunk('product/getStyles', async(productId, thunkAPI) => {
   try {
     axios.get(`${baseAPIURL}/products/${productId}/styles`).then(res => res.data);
@@ -57,23 +60,57 @@ export const productSlice = createSlice({
   name: 'product',
   initialState: initialState,
   reducers: {
-
+    // When a user clicks on the style to see, the state.currentStyle will be updated
+    setCurrentStyle: (state, action) => {
+      state.currentStyle = state.availableStyle[action.payload]
+    }
   },
   extraReducers: (builder) => {
     builder
       .addCase(getInitialData.fulfilled, (state, action) => {
-        // default set
+        // default product is the first product in the products arr
         state.productOverview = action.payload[0];
+        state.isLoading = false;
       })
       .addCase(getInitialData.rejected, (state, action) => {
         console.log(action.payload);
+        state.isLoading = false;
         state.isError = true;
+        state.errorMessage = action.payload;
       })
+      .addCase(getInitialData.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.errorMessage = '';
+      })
+
       .addCase(getSpecificProduct.fulfilled, (state, action) => {
         state.productInformation = action.payload;
+        state.isLoading = false;
       })
       .addCase(getSpecificProduct.rejected, (state, action) => {
         console.log(action.payload);
+        state.isLoading = false;
+      })
+      .addCase(getSpecificProduct.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.errorMessage = '';
+      })
+
+      .addCase(getStyles.fulfilled, (state, action) => {
+        state.availableStyles = action.payload;
+        state.currentStyle = action.payload[0];
+        state.isLoading = false;
+      })
+      .addCase(getStyles.rejected, (state, action) => {
+        console.log(action.payload);
+        state.isLoading = false;
+      })
+      .addCase(getStyles.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.errorMessage = '';
       })
   }
 })
