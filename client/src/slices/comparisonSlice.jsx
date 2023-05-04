50dec17568dcdbccee8b150872dcf3a89dd7e684
Sahid Adin <sahidadin2@gmail.com>
2023-05-04 13:39:22 -0700

    questionsSlice and component

--- client/src/slices/comparisonSlice.jsx
+++ client/src/slices/comparisonSlice.jsx
@@ -1,13 +1,1 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import axios from "axios";
-
-const initialState = {
-  question_id: '',
-  question_body : '',
-  question_date: '',
-  asker_name: '',
-  question_helpfulness: 0,
-  reported: false,
-  answers: {}
-};
 
