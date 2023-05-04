778ef7bc22245509e57c6a1147b5bf77e7bf768f
Jacqueline-Kelly <Jacqueline.kelly.2016@gmail.com>
2023-05-04 10:37:25 -0700

    pseudocode overview

---
+++ client/src/components/overview/index.jsx
@@ -0,0 +1,29 @@
+
+// actions to get started
+  // get product information
+  // get review information
+  // get style information
+
+
+// 'sub-modules' to render out
+  // product information
+    // product category
+    // product title
+    // review stars, derived from review info
+    // price, derived from style info
+    // share on socials
+  // style selector
+    // available styles displayed as thumbnails
+  // add to cart
+    // select size, derived from style info
+    // select quantity, derived from style info
+  // image gallery
+    // main image
+    // thumbnail of images to render to main
+    // zoom option
+
+
+// sub-module actions
+  // change style
+  // add to cart - select size, select quantity
+  // zoom image
