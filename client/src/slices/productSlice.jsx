5f267a648164e1154df66d045270e24797b45e2f
Jacqueline-Kelly <Jacqueline.kelly.2016@gmail.com>
2023-05-04 12:21:20 -0700

    new changes to fns

--- client/src/slices/productSlice.jsx
+++ client/src/slices/productSlice.jsx
@@ -9,32 +9,48 @@ const baseAPIURL = "https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/";
   // all styles available
   // current style selected
 
+// when getSpecificProduct is dispatched, the styles also need to be updated, and the current style needs to be set to the zeroth style in the styles array
+
 const initialState = {
-  id: '',
-  name : '',
-  slogan: '',
-  descriptions: '',
-  category: '',
-  default_price: '',
+  productOverview: {
+
+  },
+  productInformation: {
+
+  },
+  availableStyles: {
+
+  },
+  currentStyle: {
+
+  },
+  isError: false,
 };
 
+// Retrieves a list of products. API default query params gets first page and five results. Returns an array of products
 export const getInitialData = createAsyncThunk('product/getInitialData', async(_, thunkAPI) => {
   try {
-    axios.get(`${baseAPIURL}/product`).then((res) => {
-      axios.get(`${baseAPIURL}/product/${res.data.id}`).then(res => res.data)
-    })
+    axios.get(`${baseAPIURL}/products`).then((res) => res.data);
   } catch (err) {
     thunkAPI.rejectWithValue(err);
   }
 })
 
-export const getSpecificProduct = createAsyncThunk('product/getSpecificProduct', async(id, thunkAPI) => {
+export const getSpecificProduct = createAsyncThunk('product/getSpecificProduct', async(productId, thunkAPI) => {
   try {
-    axios.get(`${baseAPIURL}/product/${id}`).then(res => res.data)
+    axios.get(`${baseAPIURL}/product/${productId}`).then(res => res.data)
   } catch (err) {
     thunkAPI.rejectWithValue(err);
   }
 });
+
+export const getStyles = createAsyncThunk('product/getStyles', async(productId, thunkAPI) => {
+  try {
+    axios.get(`${baseAPIURL}/products/${productId}/styles`).then(res => res.data);
+  } catch(err) {
+    thunkAPI.rejectWithValue(err);
+  }
+})
 
 export const productSlice = createSlice({
   name: 'product',
@@ -45,17 +61,18 @@ export const productSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(getInitialData.fulfilled, (state, action) => {
-        state = action.payload;
+        // default set
+        state.productOverview = action.payload[0];
       })
       .addCase(getInitialData.rejected, (state, action) => {
         console.log(action.payload);
+        state.isError = true;
       })
       .addCase(getSpecificProduct.fulfilled, (state, action) => {
-        state = action.payload;
+        state.productInformation = action.payload;
       })
       .addCase(getSpecificProduct.rejected, (state, action) => {
         console.log(action.payload);
       })
   }
 })
-
