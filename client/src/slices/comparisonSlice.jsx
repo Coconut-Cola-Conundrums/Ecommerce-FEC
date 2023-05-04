e54e77a11c1acad49b9b66af27c8f9ddec70f38b
Jerry Renn <jerryrenn1998@gmail.com>
2023-05-04 15:04:38 -0700

    created extraReducers to handle actions dispatched by 2 new  thunk funcs

--- client/src/slices/comparisonSlice.jsx
+++ client/src/slices/comparisonSlice.jsx
@@ -1,26 +1,63 @@
+import { createSlice, createAsyncThunk } from '@redux/toolkit';
+import axios from 'axios';
 
+const initialState = {
+  relatedIds: [],
+  relatedProducts: [
+    {
+      id: '',
+      name : '',
+      slogan: '',
+      descriptions: '',
+      category: '',
+      default_price: '',
+      features: []
+    }
+  ],
+  error: null
+}
 
-import { createSlice } from '@redux/toolkit';
+const baseAPIURL = "https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp"
 
-const initialState = [
-  {
-    id: '',
-    name : '',
-    slogan: '',
-    descriptions: '',
-    category: '',
-    default_price: '',
-    features: []
+export const getRelatedIds = createAsyncThunk('products/getRelatedIds', async(id, thunkAPI) => {
+  try {
+    axios.get(`${baseAPIURL}/product/${id}/related`).then(res => res.data)
+  } catch (err) {
+    thunkAPI.rejectWithValue(err);
   }
-]
+});
+
+export const getRelatedProduct = createAsyncThunk('products/getRelatedProduct', async(id, thunkAPI) => {
+  try {
+    axios.get(`${baseAPIURL}/product/${id}`).then(res => res.data)
+  } catch (err) {
+    thunkAPI.rejectWithValue(err);
+  }
+});
+
 
 export const comparisonSlice = createSlice({
   name: 'relatedItems',
   initialState,
   reducers: {
-    update: (state, action) => {
-      return action.payload;
-    }
+
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getRelatedIds.fulfilled, (state, action) => {
+        state.relatedIds = action.payload
+      })
+      .addCase(getRelatedIds.rejected, (state, action) => {
+        console.log('error with payload: ', action.payload);
+        state.error = action.error.message;
+      })
+      .addCase(getRelatedProduct.fulfilled, (state, action) => {
+        state.relatedProducts.push(action.payload);
+      })
+      .addCase(getRelatedProduct.rejected, (state, action) => {
+        console.log('error with payload: ', action.payload);
+        state.error = action.error.message;
+      })
   }
 })
 
