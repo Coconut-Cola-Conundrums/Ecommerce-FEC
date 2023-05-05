02a328662e5e3bf46c2b7128c8000b0030f6e3e3
Jerry Renn <jerryrenn1998@gmail.com>
2023-05-04 20:55:02 -0700

    implement controller functions for routing

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,18 +1,55 @@
 const axios = require('axios');
-cosnt URL =  `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/`;
+const URL =  `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/`;
 
 module.exports = {
   getProducts: (req, res) => {
     axios.get(`${URL}/products`)
     .then(response => {
-      res.send(response.data);
+      res.json(response.data);
+      console.log('Successful getProducts call to the API');
     })
     .catch(error => {
-      console.error(error);
-      res.status(500).send('Error fetching products from API');
+      console.error('Error fetching products from API: ', error);
+      res.status(500);
     });
   },
-  getProductById: (req, res) => {},
-  getProductStylesById: (req, res) => {},
-  getRelatedProductsById: (req, res) => {},
+
+  getProductById: (req, res) => {
+    const productId = req.params.product_id;
+    axios.get(`${URL}/products/${productId}`)
+      .then(response => {
+        res.send(response.data);
+        console.log(`Sucessfully fetched product data with ID ${productId}`);
+      })
+      .catch(error => {
+        console.error(`Error fetching product with ID ${productId}`, error);
+        res.status(500)
+      });
+  },
+
+  getProductStylesById: (req, res) => {
+    const productId = req.params.product_id;
+    axios.get(`${URL}/products/${productId}/styles`)
+      .then(response => {
+        res.send(response.data);
+        console.log(`Sucessfully fetched style data from product ID ${productId}`);
+      })
+      .catch(error => {
+        console.error(`Error fetching style data with ID ${productId}`, error);
+        res.status(500)
+      });
+  },
+
+  getRelatedProductsById: (req, res) => {
+    const productId = req.params.product_id;
+    axios.get(`${URL}/products/${productId}/related`)
+      .then(response => {
+        res.send(response.data);
+        console.log(`Sucessfully fetched related products from product ID ${productId}`);
+      })
+      .catch(error => {
+        console.error(`Error fetching related products from ID ${productId}`, error);
+        res.status(500)
+      });
+  },
 };
