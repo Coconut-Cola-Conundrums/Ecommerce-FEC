714016886ffe9e6ff43b9aebe08580211909c16e
Jerry Renn <jerryrenn1998@gmail.com>
2023-05-05 22:31:08 -0700

    implement font awesome icons and more css

--- client/src/components/comparison/Card.jsx
+++ client/src/components/comparison/Card.jsx
@@ -4,13 +4,14 @@ const Card = ({ product }) => {
 
   return (
     <div className='relatedItemCard'>
-      <button>Star Button</button>
-      <div>image taken from ProductStyles</div>
+      <i className="fa-solid fa-star" style={{color: "#95a8c6"}}></i>
+      <div className="imageContainer">
+        <img className="sampleImage" src="https://cdn.shopify.com/s/files/1/2538/1942/products/Black-Blank3_1.png?v=1665434810" alt="Product Image" />
+      </div>
       <div>{product.category}</div>
-      <div>{product.name}</div>
-      <div>{product.default_price}</div>
-      <div>Product Ratings</div>
-      <div>Product default image taken from /products/:product_id/styles</div>
+      <div><strong>{product.name}</strong></div>
+      <div><small>${product.default_price}</small></div>
+      <div>Product Ratings...</div>
     </div>
   )
 }
