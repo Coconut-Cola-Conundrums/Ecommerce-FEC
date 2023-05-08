bbc1900f91ac3ee266c604a8f423ed597667f118
Michael Del Pape <m4delpape@gmail.com>
2023-05-08 14:51:52 -0700

    Added more things to the review list and completed the stars component

--- client/src/components/ratings/rating_components/rev_list.jsx
+++ client/src/components/ratings/rating_components/rev_list.jsx
@@ -1,12 +1,27 @@
 import React, {useState, useEffect} from 'react';
-
+import {Stars} from './stars.jsx';
+import {useSelector} from 'react-redux';
 
 export const RevList = (props) => {
+  const reviews = useSelector(state => state.reviews);
+
 
   return (
     <div>
       <h1>Review List</h1>
-      <div >{props.summary.map((rev) => <li key = {rev.review_id}>{rev.summary}</li>)}</div>
+      <div>
+        {reviews.reviews.map((rev) =>
+        <div key = {rev.review_id}><Stars rating = {rev.rating}/>
+        <p>From: {rev.reviewer_name} Date: {rev.date}</p>
+        Summary: {rev.summary}
+        <p>{rev.body}</p>
+        {rev.photos.map((photo) => {
+          // eslint-disable-next-line react/jsx-key
+          return <img src = {photo.url} width = '150'/>
+        })}
+
+        </div>)}
+      </div>
     </div>
   )
 }
--- client/src/components/ratings/rating_components/sort_options.jsx
+++ client/src/components/ratings/rating_components/sort_options.jsx
@@ -1,12 +1,19 @@
 import React, {useState, useEffect} from 'react';
-
+import {useSelector} from 'react-redux';
 
 export const SortOptions = () => {
+  const [search, setSearch] = useState('');
+  const reviews = useSelector(state => state.reviews);
+  console.log('search====', reviews)
+
+  var subHandler = () => {
+
+  }
 
   return (
     <div>
-      <form>
-        <input placeholder = 'Keyword'></input>
+      <form onSubmit = {subHandler}>
+        <input placeholder = 'Keyword' onChange = {(e) =>{setSearch(e.target.value)}}></input>
         <button>Search</button>
       </form>
     </div>
