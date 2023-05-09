da14c5fdb934fa67b1f11c3ae394cada7d025977
Jacqueline-Kelly <Jacqueline.kelly.2016@gmail.com>
2023-05-08 19:39:24 -0700

    thumbnail arrows adjust thumbnails in view

--- client/src/components/overview/imageGallery.jsx
+++ client/src/components/overview/imageGallery.jsx
@@ -1,15 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { FaArrowUp, FaArrowDown } from 'react-icons/fa';
 
 const ImageGallery = () => {
   const product = useSelector((state) => state.product);
   const {currentStyle} = product;
 
   const [mainPhoto, setMainPhoto] = useState(0);
+  const [thumbnailRange, setThumbnailRange] = useState({
+    from: 0,
+    to: 6
+  })
 
   const onClick = (e) => {
     e.preventDefault();
     setMainPhoto(Number(e.target.id));
+  }
+
+  const onScrollThumbnails = (e) => {
+    e.preventDefault();
+    console.log(e.target.id)
+    let direction = e.target.id === "up" ? 1 : 0;
+    console.log(direction)
+    if (!direction) { // going down, so moving TO greater indices
+      if (thumbnailRange.to <= (currentStyle.photos.length - 1)) { // if we aren't already at the highest index photo
+        setThumbnailRange((prevState) => ({...prevState, to: prevState.to+ 1}))
+      }
+    } else { // going up, so moving FROM a higher index to a lower index of thumbnails to view
+      if (thumbnailRange.from > 0) { // aren't already viewing the zeroth thumbnail
+        setThumbnailRange((prevState) => ({...prevState, from: prevState.from - 1}))
+      }
+    }
+    console.log(thumbnailRange)
   }
 
   useEffect(() => {
@@ -19,14 +41,14 @@ const ImageGallery = () => {
   return (
     <div className="photoContainer">
       {currentStyle.photos ?
-          <div>
           <div className="absolute">
             <img className="mainPhoto" src={currentStyle.photos[mainPhoto].url} alt="" />
-            {currentStyle.photos.map((photo, index) =>
+            <FaArrowUp id="up" className="thumbnailArrow" onClick={onScrollThumbnails}/>
+            {currentStyle.photos.slice(thumbnailRange.from, thumbnailRange.to).map((photo, index) =>
               <img src={photo.thumbnail_url} alt="" key={index} id={index} onClick={onClick} className={index === mainPhoto ? "thumbnail selected" : "thumbnail"}/>
             )}
+            <FaArrowDown className="thumbnailArrow" id="down" onClick={onScrollThumbnails}/>
             </div>
-          </div>
         : null
       }
     </div>
