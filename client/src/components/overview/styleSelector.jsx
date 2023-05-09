2331d42cc0b9678a0dfe8b5b4da3f88df9580ed9
Jacqueline-Kelly <Jacqueline.kelly.2016@gmail.com>
2023-05-09 11:00:51 -0700

    style arrow on selected style

--- client/src/components/overview/styleSelector.jsx
+++ client/src/components/overview/styleSelector.jsx
@@ -1,23 +1,31 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { FaCheckCircle } from 'react-icons/fa';
 
 const StyleSelector = () => {
-  const product = useSelector((state) => state.product)
+  const product = useSelector((state) => state.product);
   const {availableStyles, currentStyle} = product;
 
   useEffect(() => {
 
-  }, [availableStyles])
+  }, [availableStyles]);
 
   return (
     <div className="styleSelector">
       {product.availableStyles.length ?
-      <>
+      <div className="absolute inlineBlock">
         <h3>STYLE {currentStyle.name} </h3>
         {availableStyles.map((style, index) =>
-          <img className="styleElement" src={style.photos[0].thumbnail_url} alt="" key={index}/>
+          {return style.style_id === currentStyle.style_id ?
+            <div className="inlineBlock">
+            <FaCheckCircle id={index} style={{display:"relative", left: "2vh", top: "1vh"}}/>
+            <img className="styleElement" src={style.photos[0].thumbnail_url} alt="" key={style.product_id}/>
+            </div>
+            :
+            <img className="styleElement" src={style.photos[0].thumbnail_url} alt="" key={style.product_id}/>
+          }
         )}
-      </>
+      </div>
       : null}
     </div>
   )
