6dc6eca5e66ce505baf51b65e91501c93b125e49
Michael Del Pape <m4delpape@gmail.com>
2023-05-08 20:19:20 -0700

    Small css for review list

--- client/src/components/ratings/rating_components/rev_list.jsx
+++ client/src/components/ratings/rating_components/rev_list.jsx
@@ -2,22 +2,22 @@ import React, {useState, useEffect} from 'react';
 import {Stars} from './stars.jsx';
 import {useSelector} from 'react-redux';
 
-export const RevList = (props) => {
+export const RevList = () => {
   const reviews = useSelector(state => state.reviews);
 
 
   return (
     <div>
       <h1>Review List</h1>
-      <div>
+      <div >
         {reviews.reviews.map((rev) =>
-        <div key = {rev.review_id}><Stars rating = {rev.rating}/>
-          <p key = {rev.review_id + 1}>From: {rev.reviewer_name} Date: {rev.date}</p>
+        <div  className = 'reviewContainer' key = {rev.review_id}><Stars rating = {rev.rating}/>
+          <div>From: <p className = 'user'>{rev.reviewer_name}</p> Date: {rev.date}</div>
           Summary: {rev.summary}
-          <p key = {rev.review_id + 2}>{rev.body}</p>
+          <p>{rev.body}</p>
           {rev.photos.map((photo) => {
           // eslint-disable-next-line react/jsx-key
-          return <img key = {rev.review_id + 3} src = {photo.url} width = '150'/>
+          return <img  src = {photo.url} width = '150'/>
           })}
 
         </div>)}
