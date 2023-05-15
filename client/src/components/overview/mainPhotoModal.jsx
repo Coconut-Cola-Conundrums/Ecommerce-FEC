a27d74e28a0926531781decfc681303cc58e6e99
Jacqueline-Kelly <Jacqueline.kelly.2016@gmail.com>
2023-05-15 09:45:59 -0700

    zoom view cover

--- client/src/components/overview/mainPhotoModal.jsx
+++ client/src/components/overview/mainPhotoModal.jsx
@@ -3,17 +3,19 @@ import PropTypes from 'prop-types';
 
 const Modal = ({mainPhotoImg, show, onClickZoomPhoto, zoom }) => {
   const imgRef = useRef();
-  const [styling, setStyling] = useState({transform: "scale(2.5)", transformOrigin: "top left", objectFit: "cover", width: "100vw", cursor: "pointer", top:"5vh", position: "absolute"})
+  const [styling, setStyling] = useState({transform: "scale(2.5)", objectFit: "cover", height: "100vh", width: "100vw", cursor: "pointer", top:"5vh", position: "absolute"})
 
   const onMouse = (e) => {
     // console.log(e.clientX, e.clientY, imgRef.current.getBoundingClientRect(), e.pageX, e.pageY);
-    let { width, height, x, y, right, bottom } = imgRef.current.getBoundingClientRect();
-    // console.log('width: ', width, 'height: ', height, 'x:', x, "y:", y, "right:", right, "bottom:", bottom, e.pageX, e.pageY)
+    let { width, height, x, y, right, bottom, top, left } = imgRef.current.getBoundingClientRect();
+    console.log('width: ', width, 'height: ', height, 'x:', x, "y:", y, "right:", right, "bottom:", bottom, e.pageX, e.pageY)
     // let newStyle = {transformOrigin: `${(e.clientX - x) / width}% ${(e.clientY - y) / height}%`};
     // console.log(2.5 * 100 * e.pageX/width, 2.5 * 100 * e.pageY/height, e.pageX, e.pageY)
     // console.log(2.5 * (e.clientX - x) / width, "and y transformed is", 2.5 * (e.clientY - y / height))
     // console.log(2.5 * 100 * e.pageX/width,2.5 * 100 * (e.pageY / height))
-    console.log(`${2.5 * 100 * e.pageX/width}vw ${5 + 2.5 * 100 * (e.pageY / height)}vh`)
+    // console.log(`${2.5 * 100 * e.pageX/width}vw ${5 + 2.5 * 100 * (e.pageY / height)}vh`)
+    // setStyling(prevState => ({...prevState, transformOrigin: `${100}vw ${100}vh`}))
+    // setStyling(prevState => ({...prevState, transformOrigin: `${100}% ${100}%`}))
     setStyling(prevState => ({...prevState, transformOrigin: `${2.5 * 100 * e.pageX/width}vw ${5 + 2.5 * 100 * (e.pageY / height)}vh`}))
     // console.log(2.5 * 100 * (e.pageY -y) / height, 2.5 * 100* (e.pageX - x) /width)
     // setStyling(prevState => ({...prevState, transformOrigin: `${ 2.5 * 100 * (e.pageX - x) /width}vw ${5 + 2.5 * 100 * (e.pageY -y) / height}vh`}))
