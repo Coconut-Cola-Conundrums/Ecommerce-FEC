d44c672b431a935ce363a69fd6b8d6f0a243abdf
Jacqueline-Kelly <Jacqueline.kelly.2016@gmail.com>
2023-05-16 12:03:40 -0700

    product details tests

--- spec/overview.spec.js
+++ spec/overview.spec.js
@@ -3,9 +3,12 @@ import { render, screen, cleanup } from '@testing-library/react';
 import  userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux'
 import wrapTestWithProvider from './testStore.js';
-import  ProductDescription  from '../client/src/components/overview/productDescription';
+import ProductDescription from '../client/src/components/overview/productDescription';
+import ProductDetails from '../client/src/components/overview/productDetails';
 import StyleSelector from '../client/src/components/overview/StyleSelector';
 import { productStub, styleStub } from './stubs/productOverviewStubs.js';
+import { ratingsStub } from './stubs/ratingsStubs.js';
+
 import '@testing-library/jest-dom/extend-expect';
 
 afterEach(cleanup)
@@ -26,6 +29,15 @@ const initialState = {
   isLoading: false,
   successMessage: '',
 };
+
+const initialReviewsState = {
+  sort: 'newest',
+  allReviews: [],
+  reviews: [],
+  ratings: '',
+  recommended: '',
+  characteristics: {}
+}
 
 describe('Product Description Section', () => {
   test('Component accesses state and displays product information', async() => {
@@ -66,10 +78,45 @@ describe('Style Selector Section', () => {
     const user = userEvent.setup();
     await user.click(styleToClick);
     // new selected style should be the third in the style element array
-    screen.logTestingPlaygroundURL()
+    screen.logTestingPlaygroundURL();
     expect(await screen.queryAllByAltText("styleElement")[3]).toHaveClass("selectedStyleElement");
     expect(await screen.getByText(/digital Red & Black/i)).toBeInTheDocument();
+  });
+})
 
+describe('Product Details Section', () => {
+  const preloadedState = {
+    product:
+      {...initialState, ...productStub, ...styleStub, currentStyle: styleStub.availableStyles[0]},
+    reviews:
+      {...initialReviewsState, ...ratingsStub}
+  }
 
+  const testStore = wrapTestWithProvider(preloadedState);
+
+  test('correct category displays', async() => {
+    render(
+      <Provider store={testStore}>
+        <ProductDetails />
+      </Provider>
+    )
+    expect(await screen.queryByText(/Jackets/i)).toBeInTheDocument();
+  }),
+
+  test('user clicking on style element updates price', async() => {
+
+    render(
+      <Provider store={testStore}>
+        <ProductDetails />
+        <StyleSelector />
+      </Provider>
+    )
+
+    let styleToClick = await screen.queryAllByAltText("styleElement")[5];
+
+    const user = userEvent.setup();
+    await user.click(styleToClick);
+
+    expect(await screen.queryByText(/170/)).toBeInTheDocument();
   })
 })
