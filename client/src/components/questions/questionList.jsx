70c96437a63fcc3f445c6885251cf442296bfe2f
Sahid Adin <sahidadin2@gmail.com>
2023-05-18 11:40:21 -0700

    css

--- client/src/components/questions/questionList.jsx
+++ client/src/components/questions/questionList.jsx
@@ -1,6 +1,10 @@
 import React,{useState} from 'react';
 
+
 let QuestionList = ({questions,answers}) => {
+
+console.log(questions);
+console.log(answers);
   let changeDate = (date) => {
     const dated = new Date(date);
     const formattedDate = new Intl.DateTimeFormat('en-US', { dateStyle: 'long' }).format(dated);
@@ -8,54 +12,51 @@ let QuestionList = ({questions,answers}) => {
   }
 
   const [showMore, setShowMore] = useState([]);
+  let counter = 0;
 
  let clickHanderAnswers = () => {
-    setShowMore(!showMore);
+
+
   }
 
-//if(questions) {
-  //setShowMore(questions);
+
   return (
     <div className='QuestionsBox' data-testid="question">
-      {questions.slice(0,4).map((question)=>(
+      {questions.map((question)=>(
         <div className="questionList" key={question.question_id}>
-        <span className='qHeader'>Q: {question.question_body}</span>
-        <button>Helpful?</button>
-        |
-        <button>Add an Answer</button>
+          <span className='qHeader'>Q: {question.question_body}</span>
+          <button className='qbutton'>Helpful?</button>
+          |
+          <button className='qbutton'>Add an Answer</button>
 
-        {answers.slice(0,2).map((answer)=> (
+          {answers.map((answer)=> (
           <div key={answer.answer_id}>
-          <h3 className='answer'>A: {answer.body}</h3>
-          {answer.photos !== [] ? (
-            <div>
-            {answer.photos.map((photo)=>(
+            <h3 className='answer'>A: {answer.body}</h3>
+              <div>
+              {answer.photos.map((photo)=>(
               <img className='answerImg' key={photo.id} src={photo.url}/>
-            ))}
+              ))}
             </div>
-          ):(
-            <span></span>
-          )}
+
           <span className='credits'>by {answer.answerer_name}, {changeDate(answer.date)}</span>
-          <button>Helpful?</button>
-          <button>Report</button>
+          <button className='abutton' >Helpful?</button>
+          |
+          <button className='abutton'>Report</button>
+          <hr></hr>
           </div>
 
         ))}
         <div>
           {showMore ? (
-            <button onClick={clickHanderAnswers}>Show less</button>
+            <button className="click" onClick={clickHanderAnswers}>Show less</button>
           ):(
-            <button onClick={clickHanderAnswers}>Show more answers</button>
+            <button className="click" onClick={clickHanderAnswers}>Show more answers</button>
             )}
             </div>
         </div>
        ))}
     </div>
   )
-    //   } else {
-    //     return <h3>** No Questions **</h3>
-    //   }
   }
 
 
